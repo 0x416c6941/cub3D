437f5abd4ab9e916f5ecae49a8d4bdedc00c33f1
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-01-21 14:03:50 +0100

    parse.h: Add main function declaration

--- include/parse.h
+++ include/parse.h
@@ -6,11 +6,21 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:05:10 by asagymba          #+#    #+#             */
-/*   Updated: 2025/01/21 13:05:27 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/21 13:47:47 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
+
+# include <cub3D.h>
+
+/**
+ * Parser and saves map to \p out.
+ * @param	out	Output.
+ * @return	-1, if something went wrong. errno will also be set;
+ * 			Some non-negative value, if everything went fine.
+ */
+int	ft_parse(struct s_args *out);
 
 #endif /* PARSE_H */
