8e2c114701edc2ac663fff97e41e86035f295dc2
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-01-21 14:06:27 +0100

    parse.h: Update declaration of ft_parse() (again)

--- include/parse.h
+++ include/parse.h
@@ -6,7 +6,7 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:05:10 by asagymba          #+#    #+#             */
-/*   Updated: 2025/01/21 14:04:56 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/21 14:06:18 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,12 @@
 # include <cub3D.h>
 
 /**
- * Parses arguments and saves them to \p out.
- * @param	argc	Argument count.
- * @param	argv	Argument vector.
- * @param	out		Output.
+ * Parses file at \p file_path and saves parsed information to \p out.
+ * @param	file_path	Path to a .cub file.
+ * @param	out			Output.
  * @return	-1, if something went wrong. errno will also be set;
  * 			Some non-negative value, if everything went fine.
  */
-int	ft_parse(int argc, char **argv, struct s_args *out);
+int	ft_parse(const char *file_path, struct s_args *out);
 
 #endif /* PARSE_H */
