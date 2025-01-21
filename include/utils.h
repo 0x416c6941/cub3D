279821698df81844a3e245529d7cd5488ecc1bde
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-01-21 18:08:13 +0100

    utils/ft_get_map_row()

--- include/utils.h
+++ include/utils.h
@@ -6,7 +6,7 @@
 /*   By: hlyshchu <hlyshchu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 21:47:38 by root              #+#    #+#             */
-/*   Updated: 2025/01/21 17:54:12 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/21 18:00:09 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,16 @@
  * @return	exit_code
  */
 int		ft_errmsg(const char *msg, const char *detail, int exit_code);
+
+/**
+ * Gets a row with index \p idx from \p map.
+ * @warning	A returned row isn't a copy,
+ * 			but a pointer to a content of a map's node.
+ * @param	map	Map to get row from.
+ * @param	idx	Index of the row.
+ * @return	A row with the requested \p idx;
+ * 			NULL, if such a row doesn't exist within \p map.
+ */
+char	*ft_get_map_row(t_list *map, int idx);
 
 #endif /* UTILS_H */
