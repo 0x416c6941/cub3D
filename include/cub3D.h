b7d3a180e42d1f51d95d62448d452519dd02f37a
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-01-21 13:06:34 +0100

    cub3D.h: Use C comments

--- include/cub3D.h
+++ include/cub3D.h
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 18:56:57 by root              #+#    #+#             */
-/*   Updated: 2025/01/10 22:02:47 by root             ###   ########.fr       */
+/*   Updated: 2025/01/21 13:06:13 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <libft.h>
-
-// for system crashes: malloc, write...
+/**
+ * For system crashes: malloc, write...
+ */
 # define FATAL_ERROR -1
-
 # define ERR_USAGE "usage: ./cub3d <path/to/map.cub>"
 
-#endif // CUB3D_H
+#endif /* CUB3D_H */
