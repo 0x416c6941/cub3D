c24eb0d318db6a3d477c71090131902e223b888f
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-01-21 13:18:22 +0100

    cub3D.h: Add structures for parser

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 18:56:57 by root              #+#    #+#             */
-/*   Updated: 2025/01/21 13:06:13 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/21 13:17:35 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,46 @@
 # define CUB3D_H
 
 /**
+ * ----------------------------------------------------------------------------
+ * Constants.
+ * ----------------------------------------------------------------------------
+ */
+/**
+ * For t_args.
+ */
+/* Textures and their indexes. */
+# define TEXTURES_SIZE	4
+# define TEXTURE_NO		0
+# define TEXTURE_SO		1
+# define TEXTURE_WE		2
+# define TEXTURE_EA		3
+/* Colors and their indexes. */
+# define COLORS_SIZE	2
+# define COLOR_FLOOR	0
+# define COLOR_CEILING	1
+
+/**
  * For system crashes: malloc, write...
  */
 # define FATAL_ERROR -1
 # define ERR_USAGE "usage: ./cub3d <path/to/map.cub>"
+
+/**
+ * ----------------------------------------------------------------------------
+ * Structures.
+ * ----------------------------------------------------------------------------
+ */
+typedef struct s_rgb
+{
+	unsigned short	r;
+	unsigned short	g;
+	unsigned short	b;
+}	t_rgb;
+
+typedef struct s_args
+{
+	char	*textures[TEXTURES_SIZE];
+	t_rgb	colors[COLORS_SIZE];
+}	t_args;
 
 #endif /* CUB3D_H */
