72799bfb22cf735b50f24caf8a2974f3394f59f4
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-01-21 14:10:04 +0100

    main.c: use ft_parse()

--- src/main.c
+++ src/main.c
@@ -6,16 +6,22 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 18:44:24 by root              #+#    #+#             */
-/*   Updated: 2025/01/20 15:26:54 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/21 14:09:12 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 #include <utils.h>
+#include <parse.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int	main(int argc, char **argv)
 {
-	(void)argv;
+	struct s_args	args;
+
 	if (argc != 2)
 		return (ft_errmsg("invalid number of parameters", ERR_USAGE, 2));
+	else if (ft_parse(*(++argv), &args) == -1)
+		return (perror("cub3D: "), EXIT_FAILURE);
 }
