7a6a7f3893037823ba71d5daaf2ed6dc32c36782
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-01-21 14:11:27 +0100

    parse.h: initial commit

---
+++ src/parse/ft_parse.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 14:10:25 by asagymba          #+#    #+#             */
+/*   Updated: 2025/01/21 14:10:51 by asagymba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <parse.h>
+#include <cub3D.h>
+#include <libft.h>
+
+int	ft_parse(const char *file_path, struct s_args *out)
+{
+}
