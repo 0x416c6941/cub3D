dd3c941cb7527e5acb1b664e9c159ad56a1696bd
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-01-22 18:51:06 +0100

    utils: Restructure

--- Makefile
+++ Makefile
@@ -10,13 +10,11 @@ INC_DIR = include
 # Source files.
 SRC_DIR = src
 SRC_FILES = main.c							\
-		utils/ft_errmsg.c utils/ft_get_map_row.c		\
-		utils/ft_mlx_exit.c					\
+	    	utils.c							\
 		parse/ft_parse.c					\
-		init/ft_free_s_args_content.c				\
-		init/ft_calculate_angle_deltas.c init/ft_prep_data.c	\
-		init/ft_free_s_data_content.c				\
-		controls.c
+		init/init.c init/free_content.c init/utils.c		\
+		controls.c						\
+		render/ft_render.c
 
 SRC_FILES := $(addprefix $(SRC_DIR)/,$(SRC_FILES))
 
--- src/utils.c
+++ src/utils.c
@@ -1,29 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_errmsg.c                                        :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 21:42:28 by root              #+#    #+#             */
-/*   Updated: 2025/01/21 17:54:32 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/22 18:50:45 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <utils.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <libft.h>
+#include <stddef.h>
+#include <cub3D.h>
+#include <init.h>
+#include <stdlib.h>
 
-/**
- * @brief Add prefix "cub3D: Error" and print to standard error output.
- *  err_msg and detail can be printed to specify the error. Return the exit code
- * passed as a parameter.
- *
- * @param msg
- * @param detail
- * @param exit_code
- * @return int exit_code
- */
 int	ft_errmsg(const char *msg, const char *detail, int exit_code)
 {
 	if ((write(STDERR_FILENO, CUB_PREFIX, ft_strlen(CUB_PREFIX)) == -1)
@@ -47,4 +42,23 @@ int	ft_errmsg(const char *msg, const char *detail, int exit_code)
 		return (-1);
 	}
 	return (exit_code);
+}
+
+char	*ft_get_map_row(t_list *map, int idx)
+{
+	t_list	*node;
+
+	if (idx < 0)
+		return (NULL);
+	node = ft_lstat(map, (size_t)idx);
+	if (node == NULL)
+		return (NULL);
+	return (node->content);
+}
+
+int	ft_mlx_exit(struct s_data *data, int exit_code)
+{
+	ft_free_s_data_content(data);
+	exit(exit_code);
+	return (-1);
 }
