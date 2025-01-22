eef0b5dc6e50f3c18a6ea6cbd724ad0fe334faec
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-01-22 19:26:32 +0100

    render: Allocate image for rendering

--- include/render.h
+++ include/render.h
@@ -6,7 +6,7 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:13:59 by asagymba          #+#    #+#             */
-/*   Updated: 2025/01/22 18:32:50 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/22 19:22:46 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@
  * ----------------------------------------------------------------------------
  */
 /**
- * Needed for accuracy.
+ * Needed for precision.
  */
 # define FOR_ACC	0.0001
 /**
- * When casting rays, 
+ * Used for casting rays.
  */
 # define RAY_OFFSET	64
 
@@ -34,6 +34,23 @@
  * Structures.
  * ----------------------------------------------------------------------------
  */
+/**
+ * Data required for renderer to work.
+ */
+typedef struct s_render
+{
+	/**
+	 * MLX stuff.
+	 */
+	void	*image;
+	char	*image_pixels;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endianness;		/* We don't care about it tho. */
+	/**
+	 * For ray casting.
+	 */
+}	t_render;
 
 /**
  * ----------------------------------------------------------------------------
@@ -43,7 +60,8 @@
 /**
  * Renders the image. If something goes wrong - exits the program.
  * @param	data	cub3D's data.
- * @return	Always 0. Safe to ignore.
+ * @return	Safe to ignore. Some non-negative value if everything went fine;
+ * 			-1 on error (error log will be written and program will exit).
  */
 int	ft_render(struct s_data *data);
 
--- src/render/ft_render.c
+++ src/render/ft_render.c
@@ -6,15 +6,34 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:15:20 by asagymba          #+#    #+#             */
-/*   Updated: 2025/01/22 18:38:20 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/22 19:25:19 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <render.h>
 #include <cub3D.h>
+#include <mlx.h>
+#include <stddef.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <utils.h>
 
 int	ft_render(struct s_data *data)
 {
+	t_render	render_data;
+
 	(void)data;
+	render_data.image = mlx_new_image(data->conn, WIN_X, WIN_Y);
+	if (render_data.image == NULL)
+	{
+		if (errno == 0)
+			errno = EACCES;
+		return (perror("cub3D"), ft_mlx_exit(data, EXIT_FAILURE), -1);
+	}
+	render_data.image_pixels = mlx_get_data_addr(render_data.image,
+			&render_data.bits_per_pixel, &render_data.size_line,
+			&render_data.endianness);
+	(void)mlx_destroy_image(data->conn, render_data.image);
 	return (0);
 }
