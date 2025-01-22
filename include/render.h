6dd356047b6b1fbe6392932f180c36b92426bf6b
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-01-23 00:13:19 +0100

    render/ft_pixel_get_from_image()

--- include/render.h
+++ include/render.h
@@ -6,7 +6,7 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:13:59 by asagymba          #+#    #+#             */
-/*   Updated: 2025/01/22 23:28:28 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/23 00:12:35 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@
  * @return	Safe to ignore. Some non-negative value if everything went fine;
  * 			-1 on error (error log will be written and program will exit).
  */
-int		ft_render(struct s_data *data);
+int				ft_render(struct s_data *data);
 
 /**
  * Draws a \p pixel to a point [\p x, \p y] in \p img.
@@ -45,7 +45,16 @@ int		ft_render(struct s_data *data);
  * @param	y		y coordinate.
  * @param	pixel	Pixel to draw.
  */
-void	ft_pixel_put_on_image(struct s_img *img, int x, int y,
-			const struct s_rgb *pixel);
+void			ft_pixel_put_on_image(struct s_img *img, int x, int y,
+					const struct s_rgb *pixel);
+
+/**
+ * Gets pixel from \p img with coordinates \p x and \p y.
+ * @param	img		Image to get pixel from.
+ * @param	x		x coordinate.
+ * @param	y		y coordinate.
+ * @return	Requested pixed, or black pixel if coordinates are out of bounds.
+ */
+struct s_rgb	ft_pixel_get_from_image(struct s_img *img, int x, int y);
 
 #endif /* RENDER_H */
--- src/render/utils.c
+++ src/render/utils.c
@@ -6,7 +6,7 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 23:06:29 by asagymba          #+#    #+#             */
-/*   Updated: 2025/01/22 23:39:17 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/23 00:09:41 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,22 @@ void	ft_pixel_put_on_image(struct s_img *img, int x, int y,
 	to_put <<= OCTET;
 	to_put += pixel->b;
 	*((int *)(img->img_pixels + offset)) = to_put;
+}
+
+struct s_rgb	ft_pixel_get_from_image(struct s_img *img, int x, int y)
+{
+	const struct s_rgb	black = {0, 0, 0};
+	struct s_rgb		ret;
+	int					offset;
+	unsigned char		*pixel;
+
+	if (x >= img->width || y >= img->height)
+		return (black);
+	(void)ft_memset(&ret, 0, sizeof(struct s_rgb));
+	offset = (x * (img->bits_per_pixel / 8)) + (img->size_line * y);
+	pixel = ((unsigned char *)(img->img_pixels + offset));
+	ret.r = *(++pixel);
+	ret.g = *(++pixel);
+	ret.b = *(++pixel);
+	return (ret);
 }
