a999c3f5be84aeaacd4e0eee9d5625ee0f1e7068
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-01-22 23:39:42 +0100

    render/utils.c: Norme

--- src/render/utils.c
+++ src/render/utils.c
@@ -6,7 +6,7 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 23:06:29 by asagymba          #+#    #+#             */
-/*   Updated: 2025/01/22 23:35:27 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/22 23:39:17 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@
 void	ft_pixel_put_on_image(struct s_img *img, int x, int y,
 			const struct s_rgb *pixel)
 {
-	int	offset, to_put;
+	int	offset;
+	int	to_put;
 
 	offset = (x * (img->bits_per_pixel / 8)) + (img->size_line * y);
 	to_put = pixel->r;
