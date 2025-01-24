e65f97f7786da4f0e6f206fd5e616217b47f2721
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-01-24 02:26:46 +0100

    main.c: Use ft_parse

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 18:44:24 by root              #+#    #+#             */
-/*   Updated: 2025/01/23 15:45:11 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/01/24 02:20:05 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,67 +24,6 @@
 #include <stddef.h>
 
 /**
- * Norminette bypass.
- * @param	args	Args to initialize.
- * @return	-1, if something went wrong (errno will NOT be set);
- * 			Some non-negative value otherwise.
- */
-static int	ft_tmp_init_args_norminette(struct s_args *args)
-{
-	const char *const	rows[] = {"11111", "1S001", "10101", "11111", NULL};
-	size_t				i;
-	char				*row;
-	t_list				*node;
-
-	args->map = NULL;
-	i = 0;
-	while (rows[i] != NULL)
-	{
-		row = ft_strdup(rows[i]);
-		if (row == NULL)
-			return (ft_lstclear(&args->map, free), -1);
-		node = ft_lstnew(row);
-		if (node == NULL)
-			return (free(row), ft_lstclear(&args->map, free), -1);
-		ft_lstadd_back(&args->map, node);
-		i++;
-	}
-	return (0);
-}
-
-/**
- * Temporarily initialises \p args, as parser isn't ready yet.
- * @param	args	Args to initialize.
- * @return	-1, if something went wrong (errno will NOT be set);
- * 			Some non-negative value otherwise.
- */
-static int	ft_tmp_init_args(struct s_args *args)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < TEXTURES_SIZE)
-	{
-		args->textures[i] = ft_strdup("textures/simonkraft/netherrack.xpm");
-		if (args->textures[i] == NULL)
-		{
-			j = 0;
-			while (j < i)
-			{
-				free(args->textures[j]);
-				args->textures[j++] = NULL;
-			}
-			return (-1);
-		}
-		i++;
-	}
-	args->colors[COLOR_FLOOR] = (struct s_rgb){50, 50, 50, true};
-	args->colors[COLOR_CEILING] = (struct s_rgb){100, 100, 100, true};
-	return (ft_tmp_init_args_norminette(args));
-}
-
-/**
  * Add this when parser is finished:
 	if (ft_parse(*(++argv), &args) == -1
 		|| ft_prep_data(&args, &data) == -1)
@@ -99,11 +38,8 @@ int	main(int argc, char **argv)
 		return (ft_errmsg("invalid number of parameters", ERR_USAGE, 2));
 	(void)argv;
 	(void)ft_memset(&args, 0, sizeof(struct s_args));
-	if (ft_tmp_init_args(&args) == -1)
-	{
-		return (ft_errmsg("Something went wrong in ft_tmp_init_args()",
-				NULL, 1));
-	}
+	if (ft_parse(*(++argv), &args) != 0)
+		return (ft_free_s_args_content(&args), EXIT_FAILURE);
 	(void)ft_memset(&data, 0, sizeof(struct s_data));
 	if (ft_prep_data(&args, &data) == -1)
 		return (perror("cub3D"), ft_free_s_args_content(&args),
